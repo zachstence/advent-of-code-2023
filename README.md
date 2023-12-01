3c208032222c4c89db5966bb2d3b31c90a35afcf
Zachary Stence <zachstence@gmail.com>
2023-12-01 11:04:12 -0600

    feat(day1): part 2 complete

--- README.md
+++ README.md
@@ -11,7 +11,7 @@
 
 <!--SOLUTIONS-->
 
-[![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%86/yellow)](src/day01)
+[![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 ![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
@@ -69,9 +69,9 @@ npm start 1
 
 ```
 Day 01
-Time part 1: 1.345ms
-Time part 2: -
-Both parts: 1.345ms
+Time part 1: 1.323ms
+Time part 2: 22.454ms
+Both parts: 23.777ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 1/50
-Total time: 1.345ms
+Total stars: 2/50
+Total time: 23.777ms
 ```
 
 <!--/RESULTS-->
--- src/day01/index.ts
+++ src/day01/index.ts
@@ -1,4 +1,17 @@
 import run from "aocrunner";
+import console from "console";
+
+const WORD_TO_NUMBER: Record<string, string> = {
+  'one': '1',
+  'two': '2',
+  'three': '3',
+  'four': '4',
+  'five': '5',
+  'six': '6',
+  'seven': '7',
+  'eight': '8',
+  'nine': '9',
+}
 
 const parseInput = (rawInput: string): string[] => rawInput.split('\n');
 
@@ -11,7 +24,7 @@ const part1 = (rawInput: string): string => {
     const first = chars.find(s => !isNaN(parseInt(s)))
     const last = chars.findLast(s => !isNaN(parseInt(s)))
     if (!first || !last) {
-      console.error(`Failed to find first and last numbers in ${line}`)
+      console.error(`Failed to find first and last numbers in '${line}'`)
       return sum
     }
 
@@ -24,9 +37,26 @@ const part1 = (rawInput: string): string => {
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+  const input = parseInput(rawInput)
 
-  return;
+  const sum = input.reduce<number>((sum, line) => {
+    const matches = [...line.matchAll(/(?=(one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9))/gi)]
+
+    const firstMatch = matches[0]
+    const lastMatch = matches[matches.length - 1]
+
+    const firstMatchStr = firstMatch[1]
+    const firstMatchNumber = firstMatchStr.length === 1 ? firstMatchStr : WORD_TO_NUMBER[firstMatchStr]
+
+    const lastMatchStr = lastMatch[1]
+    const lastMatchNumber = lastMatchStr.length === 1 ? lastMatchStr : WORD_TO_NUMBER[lastMatchStr]
+
+    const lineValue = parseInt(`${firstMatchNumber}${lastMatchNumber}`)
+
+    return sum + lineValue
+  }, 0)
+
+  return sum.toString()
 };
 
 run({
@@ -69,10 +99,46 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `two1asdjfahsdfjhkagsdjhfg2`,
+        expected: "22",
+      },
+      {
+        input: `asfasdfsfasdfasnineasfasfasdfasdfa`,
+        expected: "99",
+      },
+      {
+        input: `asdfeightfeightasdfasdfasdfasdf`,
+        expected: "88",
+      },
+      {
+        input: `1`,
+        expected: "11",
+      },
+      {
+        input: `1onetwothree2`,
+        expected: "12",
+      },
+      {
+        input: `one2222222222nine222222222221`,
+        expected: "11",
+      },
+      {
+        input: `3sdthreefasdfsfasfasdfasdfas`,
+        expected: "33",
+      },
+      {
+        input: `asdfasdf7assixf6asdfa6sdfeightas6fasfivedfa54sd3f56asd3f48as67dtfasd6f545asd3f5as3d46f6a5s8df68as56d8f4asd6f4as6789sixdf`,
+        expected: "76",
+      },
+      {
+        input: 'twoneightwonelevenineight',
+        expected: '28',
+      },
+      {
+        input: 'nineight',
+        expected: '98',
+      },
     ],
     solution: part2,
   },
